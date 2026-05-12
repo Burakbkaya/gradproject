012b99b20766c19da067f90e3193b821992fb39d
Burakbkaya <brkbkya@hotmail.com>
2026-05-13 02:33:53 +0300

    Pre-download BERT model during build to avoid Render request timeout

--- render-build.sh
+++ render-build.sh
@@ -4,3 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 playwright install chromium
+
+# Modelin ilk istekte inmesini ve zaman aşımına sebep olmasını engellemek için build aşamasında indiriyoruz:
+python -c "from transformers import pipeline; pipeline('sentiment-analysis', model='savasy/bert-base-turkish-sentiment-cased')"
